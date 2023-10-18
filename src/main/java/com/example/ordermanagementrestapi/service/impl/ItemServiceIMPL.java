19fb0fd178552c32baf37815d208db563f4e4561
KAMoviniMudara <mudaramovini@gmail.com>
2023-10-18 06:39:07 +0530

    Complete Login

--- src/main/java/com/example/ordermanagementrestapi/service/impl/ItemServiceIMPL.java
+++ src/main/java/com/example/ordermanagementrestapi/service/impl/ItemServiceIMPL.java
@@ -22,41 +22,32 @@ public class ItemServiceIMPL implements ItemService {
     @Autowired
     private ModelMapper modelMapper;
 
-
+    @Autowired
     private ItemMapper itemMapper;
-
 
     @Override
     public void addCustomer(RequestItemSaveDTO requestItemSaveDTO) {
-
         Item item = itemMapper.requestDtoToEntity(requestItemSaveDTO);
         item.setActiveState(false);
-        if(!itemRepo.existsById(item.getItemID())){
+        if (!itemRepo.existsById(item.getItemID())) {
             itemRepo.save(item);
-            System.out.println("Saved");
         }
-
     }
 
     @Override
     public List<ItemDTO> getItemByName(String itemName) {
-
         List<Item> items = itemRepo.findAllByItemName(itemName);
-
         List<ItemDTO> itemDTOS = itemMapper.requestEntityListToDtoList(items);
-
         return itemDTOS;
     }
 
     @Override
     public List<ItemDTO> getAllItems() {
-
         List<Item> items = itemRepo.findAllByActiveStateIs(false);
-
-        if(items.size()>0) {
+        if (items.size() > 0) {
             List<ItemDTO> itemDTOS = itemMapper.requestEntityListToDtoList(items);
             return itemDTOS;
-        }else {
+        } else {
             throw new NotFoundException("No data found");
         }
     }
@@ -64,12 +55,10 @@ public class ItemServiceIMPL implements ItemService {
     @Override
     public String updateItemByName(ItemDTO itemDTO) {
         Item existingItem = itemRepo.findByItemName(itemDTO.getItemName());
-
         if (existingItem != null) {
             existingItem.setBalanceQty(itemDTO.getBalanceQty());
             existingItem.setSupplierPrice(itemDTO.getSupplierPrice());
             existingItem.setSellerPrice(itemDTO.getSellerPrice());
-
             itemRepo.save(existingItem);
             return "Updated";
         } else {
@@ -80,15 +69,12 @@ public class ItemServiceIMPL implements ItemService {
     @Override
     public String deactivateItemByName(String itemName) {
         try {
-            Item item= itemRepo.findByItemName(itemName);
-
+            Item item = itemRepo.findByItemName(itemName);
             if (item == null) {
                 return "Item Not Found";
             }
-
             item.setActiveState(false);
             itemRepo.save(item);
-
             return "Item Deactivated";
         } catch (Exception e) {
             return "Deactivation Failed";
@@ -98,7 +84,6 @@ public class ItemServiceIMPL implements ItemService {
     @Override
     public String activateItemByName(String itemName) {
         Item item = itemRepo.findByItemName(itemName);
-
         if (item != null) {
             item.setActiveState(true);
             itemRepo.save(item);
@@ -109,13 +94,10 @@ public class ItemServiceIMPL implements ItemService {
     }
 
     public List<String> getAllItemNames() {
-        List<Item> items = itemRepo.findAll(); // Fetch all items from the database
+        List<Item> items = itemRepo.findAll();
         return items.stream()
-                .map(Item::getItemName) // Extract item names from items
+                .map(Item::getItemName)
                 .collect(Collectors.toList());
     }
-
-
-
-
 }
+
