9e379ded957c9178bad38a2d8e8a160b78b23cdb
KAMoviniMudara <mudaramovini@gmail.com>
2023-10-19 16:19:09 +0530

    Add SLF4J dependency

--- src/main/java/com/example/ordermanagementrestapi/controller/UserController.java
+++ src/main/java/com/example/ordermanagementrestapi/controller/UserController.java
@@ -4,34 +4,38 @@ import com.example.ordermanagementrestapi.Response.LoginResponse;
 import com.example.ordermanagementrestapi.dto.LoginDto;
 import com.example.ordermanagementrestapi.dto.UserDto;
 import com.example.ordermanagementrestapi.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
 
     @PostMapping(path = "/save")
-    public String saveUser(@RequestBody UserDto userDto)
-    {
+    public String saveUser(@RequestBody UserDto userDto) {
         String id = userService.addUser(userDto);
-        return id;
 
+        // Log a message when a user is saved
+        logger.info("User saved with ID: {}", id);
+
+        return id;
     }
 
     @PostMapping(path = "login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginDto loginDto)
-    {
+    public ResponseEntity<?> loginUser(@RequestBody LoginDto loginDto) {
         LoginResponse loginResponse = userService.loginUser(loginDto);
+
+        // Log a message when a user logs in
+        logger.info("User logged in with email: {}", loginDto.getEmail());
+
         return ResponseEntity.ok(loginResponse);
     }
-
 }
-
-
--- src/main/java/com/example/ordermanagementrestapi/dto/UserDto.java
+++ src/main/java/com/example/ordermanagementrestapi/dto/UserDto.java
@@ -1,65 +1,67 @@
 package com.example.ordermanagementrestapi.dto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UserDto {
+    private static final Logger logger = LoggerFactory.getLogger(UserDto.class);
 
-        private int userId;
+    private int userId;
+    private String userName;
+    private String email;
+    private String password;
 
-        private String userName;
+    public UserDto(int userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
 
-        private String email;
 
-        private String password;
+    }
 
-        public UserDto(int userId, String userName, String email, String password) {
-            this.userId = userId;
-            this.userName = userName;
-            this.email = email;
-            this.password = password;
-        }
+    public UserDto() {
+    }
 
-        public UserDto() {
-        }
+    public int getUserId() {
+        return userId;
+    }
 
-        public int getUserId() {
-            return userId;
-        }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-        public void setUserId(int userId) {
-            this.userId = userId;
-        }
+    public String getUserName() {
+        return userName;
+    }
 
-        public String getUserName() {
-            return userName;
-        }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public String getPassword() {
-            return password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        @Override
-        public String toString() {
-            return "UserDto{" +
-                    "userId=" + userId +
-                    ", userName='" + userName + '\'' +
-                    ", email='" + email + '\'' +
-                    ", password='" + password + '\'' +
-                    '}';
-        }
-
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
