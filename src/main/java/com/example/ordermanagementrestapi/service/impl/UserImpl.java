e9360a578917daf03423003c390d908ad525d2e8
KAMoviniMudara <mudaramovini@gmail.com>
2023-10-23 09:14:09 +0530

    Add SLF4J for seviceIMPL layers

--- src/main/java/com/example/ordermanagementrestapi/service/impl/UserImpl.java
+++ src/main/java/com/example/ordermanagementrestapi/service/impl/UserImpl.java
@@ -5,6 +5,8 @@ import com.example.ordermanagementrestapi.dto.LoginDto;
 import com.example.ordermanagementrestapi.dto.UserDto;
 import com.example.ordermanagementrestapi.repo.UserRepo;
 import com.example.ordermanagementrestapi.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ import java.util.Optional;
 
 @Service
 public class UserImpl implements UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserImpl.class);
+
     @Autowired
     private UserRepo userRepo;
 
@@ -29,6 +33,7 @@ public class UserImpl implements UserService {
         );
 
         userRepo.save(user);
+        logger.info("User added: {}", user.getUserName());
         return user.getUserName();
     }
 
@@ -42,14 +47,18 @@ public class UserImpl implements UserService {
             if (isPwdRight) {
                 Optional<com.example.ordermanagementrestapi.entity.User> user = userRepo.findOneByEmailAndPassword(loginDto.getEmail(), encodedPassword);
                 if (user.isPresent()) {
+                    logger.info("User logged in: {}", user1.getUserName());
                     return new LoginResponse("Login Success", true);
                 } else {
+                    logger.warn("Login failed for user: {}", user1.getUserName());
                     return new LoginResponse("Login Failed", false);
                 }
             } else {
+                logger.warn("Password mismatch for user: {}", user1.getUserName());
                 return new LoginResponse("Password Not Match", false);
             }
         } else {
+            logger.warn("Email not found: {}", loginDto.getEmail());
             return new LoginResponse("Email not exists", false);
         }
     }
